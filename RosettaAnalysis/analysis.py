be9c4d1f1d48c4fac046c42584166fcdaaea5312
Sai Pooja Mahajan <saipooja@gmail.com>
2018-11-08 14:21:49 -0500

    fixed error

--- RosettaAnalysis/analysis.py
+++ RosettaAnalysis/analysis.py
@@ -6,59 +6,65 @@ pyrosetta.init('-include_sugars')
 #Setting up mover for viewing structures
 pm = PyMOLMover() # 1. Different from tutorial
 import argparse
-from rosetta.core.pose import pose_from_saccharide_sequence
+from rosetta.core.pose import *
+# pose_from_saccharide_sequence
 from rosetta.core.scoring import *
 
 
 def CalculateRMSDCA(pose_mod,pose_ref):
-	print "rmsd ca"
+	#print "rmsd ca"
 	rmsd = CA_rmsd(pose_mod,pose_ref)
-	print rmsd
+	#print rmsd
+	return rmsd
 
 def CalculateRMSDProteinOnly():
 	print "rmsd protein only"
 
 def CalculateRMSDHeavyAtoms(pose_mod,pose_ref):
-	print "rmsd heavy atoms"
+	#print "rmsd heavy atoms"
 	rmsd = rms_at_corresponding_heavy_atoms(pose_mod,pose_ref)
-	print rmsd
+	#print rmsd
+	return rmsd
 
 def CalculateRMSDSelection():
 	print "rmsd selection"
 
 def CalculateRMSDAll():
-	CalculateRMSDCA()
-	CalculateRMSDProteinOnly()
+	rmsdca = CalculateRMSDCA()
+	rmsdha = CalculateRMSDProteinOnly()
 
 def GetRMSDForFile(filename,pose_ref,rmsdtype):
-	print "Getting rmsd for file", filename
+	#print "Getting rmsd for file", filename
 	if os.path.exists(filename):
 		pose_compare = pose_from_pdb(filename)
-	if rmsdtype=='rmsd_ca':
-                CalculateRMSDCA(pose_compare,pose_ref)
-        if rmsdtype=='rmsd_ha':
-                CalculateRMSDHeavyAtoms(pose_compare,pose_ref)	
+		rmsd='-1.0'
+		if rmsdtype=='rmsd_ca':
+                	rmsd = CalculateRMSDCA(pose_compare,pose_ref)
+        	if rmsdtype=='rmsd_ha':
+                	rmsd = CalculateRMSDHeavyAtoms(pose_compare,pose_ref)
+		return rmsd
 
-def GetRMSDForList(filelist,pose_ref,rmsdtype):
+def GetRMSDForList(filelist,pose_ref,rmsdtype,fout):
 	f=open(filelist,'r')
 	for line in f:
 		filename=line.split()[0]
 		if os.path.exists(filename):
-			GetRMSDForFile(filename,pose_ref,rmsdtype)
-			 
+			rmsd = GetRMSDForFile(filename,pose_ref,rmsdtype)
+			fout.write(filename+'\t%06.4f\n' %rmsd)
 
 if __name__ ==  "__main__":
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--type_calc', help='type of rmsd calculation, options are rmsd_ca')
 	parser.add_argument('--native', help='reference pdb',default='native.pdb')
 	parser.add_argument('--traj', action='store_false', help='process list of files')
-	parser.add_ardument('--file', action='store_true', help='process single file')
+	parser.add_argument('--file', action='store_true', help='process single file')
 	parser.add_argument('--singlefile', help='pdb file name for rmsd calculation', default='input.pdb')
 	parser.add_argument('--filelist', help='list of pdb files for rmsd calculation', default='list.txt')
 	
 	args = parser.parse_args()
 	print args
-	pose_ref = pose()
+	
+	pose_ref = Pose()
 	if os.path.exists(args.native):
 		print "Native file: ",args.native, " used for comparison"
 		pose_ref = pose_from_pdb(args.native)
@@ -68,12 +74,17 @@ if __name__ ==  "__main__":
 	if args.file:
 		filename=args.singlefile
 		if os.path.exists(singlefile):
-			GetRMSDForFile(filename,pose_ref,args.type_calc)
+			rmsd = GetRMSDForFile(filename,pose_ref,args.type_calc)
+			print "\nNative: ",args.native, "\nComparison: ",srgs.singlefile," \nRMSD: ",rmsd
 		else:
 			print "provide filename for calculation with --singlefile option. Use --help for more details."
 	if args.traj:
 		filelist=args.filelist
 		if os.path.exists(filelist):
-			GetRMSDForList(filelist,pose_ref,args.type_calc)
+			outfilename = filelist + "_rmsd.out"
+			fout = open(outfilename,'w')
+			fout.write('#%s\n' %args)
+			GetRMSDForList(filelist,pose_ref,args.type_calc,fout)
+			fout.close()
 		else:
 			print "provide filename for list of pdbs with --filelist option. Use --help for more details."
